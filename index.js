ba9811c92e69eb969e52c125f7dc1abcd5d9b82d
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2023-12-14 06:10:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ';
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
